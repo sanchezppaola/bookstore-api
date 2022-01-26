26870ede8b51eaf5d03fbcb19bb2330c46b13d9b
paola <paola@itout.com.br>
2022-01-26 16:37:27 -0300

    repositories

---
+++ src/main/java/com/paola/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.paola.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paola.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
