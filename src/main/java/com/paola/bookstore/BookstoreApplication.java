d2a70892a4f46c05225c174cd587939fcd7689b0
paola <paola@itout.com.br>
2022-01-27 18:14:30 -0300

    properties test

--- src/main/java/com/paola/bookstore/BookstoreApplication.java
+++ src/main/java/com/paola/bookstore/BookstoreApplication.java
@@ -1,51 +1,32 @@
 package com.paola.bookstore;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 
 import org.h2.tools.Server;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.paola.bookstore.domain.Categoria;
-import com.paola.bookstore.domain.Livro;
-import com.paola.bookstore.repositories.CategoriaRepository;
-import com.paola.bookstore.repositories.LivroRepository;
+
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication {
 
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+	 @Bean(initMethod = "start", destroyMethod = "stop")
+	    public Server h2Server() throws SQLException {
+	        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	  
+	 } 
 	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
 	
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server h2Server() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	
+  
     }
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
-	}
-
 }
+
+
+
+
---
+++ src/main/java/com/paola/bookstore/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.paola.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.paola.bookstore.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		dbService.instanciaBaseDeDados();
+		
+	}
+	
+	
+}
