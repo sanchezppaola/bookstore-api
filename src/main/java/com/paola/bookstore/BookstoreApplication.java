ad559c1555c6ce2e34dcc2c1094686ddac19e779
paola <paola@itout.com.br>
2022-01-28 12:02:03 -0300

    db h2 para mysql

--- src/main/java/com/paola/bookstore/BookstoreApplication.java
+++ src/main/java/com/paola/bookstore/BookstoreApplication.java
@@ -1,22 +1,18 @@
 package com.paola.bookstore;
 
-import java.sql.SQLException;
-
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 
 @SpringBootApplication
 public class BookstoreApplication {
 
-	 @Bean(initMethod = "start", destroyMethod = "stop")
-	    public Server h2Server() throws SQLException {
-	        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
-	  
-	 } 
+//	 @Bean(initMethod = "start", destroyMethod = "stop")
+//	    public Server h2Server() throws SQLException {
+//	        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+//	  
+//	 } 
 	
 	
 	public static void main(String[] args) {
