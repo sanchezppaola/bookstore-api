92b015b967721a74d730247a3b282668e700c021
paola <paola@itout.com.br>
2022-01-29 11:05:24 -0300

    findById

---
+++ src/main/java/com/paola/bookstore/service/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.paola.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paola.bookstore.domain.Categoria;
+import com.paola.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+		
+		
+	}
+}
